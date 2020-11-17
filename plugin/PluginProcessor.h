922291302304c29ca827f9b5cc766e2e44474913
HolyBimBam <72328707+HolyBimBam@users.noreply.github.com>
2020-11-17 23:14:28 +0100

    add Parameter
    was very happy to find somebody that also wanted to work have CloudSeed working on Mac etc... 👍 
    
    I've added all Parameter so they can be edited via the editor view in a DAW.
    Also edited .cpp file

--- plugin/PluginProcessor.h
+++ plugin/PluginProcessor.h
@@ -43,7 +43,65 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
-	CloudSeed::ReverbController reverb;
+    CloudSeed::ReverbController reverb;
+	
+    juce::AudioParameterFloat* inputMix;
+    juce::AudioParameterFloat* lowPass;
+    juce::AudioParameterFloat* highPass;
+    juce::AudioParameterFloat* preDelay;
+
+    juce::AudioParameterFloat* dryOutGain;
+    juce::AudioParameterFloat* predelayOutGain;
+    juce::AudioParameterFloat* earlyOutGain;
+    juce::AudioParameterFloat* mainOutGain;
+    
+    
+    juce::AudioParameterFloat* tapCount;
+    juce::AudioParameterFloat* tapLength;
+    juce::AudioParameterFloat* tapGain;
+    juce::AudioParameterFloat* tapDecay;
+
+    juce::AudioParameterFloat* lateDiffusionEnabled;
+    juce::AudioParameterFloat* diffusionEnabled;
+    juce::AudioParameterFloat* diffusionStages;
+    juce::AudioParameterFloat* diffusionDelay;
+    juce::AudioParameterFloat* diffusionFeedback;
+    juce::AudioParameterFloat* earlyDiffusionModAmount;
+    juce::AudioParameterFloat* earlyDiffusionModRate;
+    
+    juce::AudioParameterFloat* lineCount;
+    juce::AudioParameterFloat* lineDelay;
+    juce::AudioParameterFloat* lineDecay;
+    juce::AudioParameterFloat* lineModAmount;
+    juce::AudioParameterFloat* lineModRate;
+    
+    
+    juce::AudioParameterFloat* lateDiffusionStages;
+    juce::AudioParameterFloat* lateDiffusionDelay;
+    juce::AudioParameterFloat* lateDiffusionFeedback;
+    juce::AudioParameterFloat* lateDiffusionModAmount;
+    juce::AudioParameterFloat* lateDiffusionModRate;
+    
+    juce::AudioParameterFloat* postLowShelfGain;
+    juce::AudioParameterFloat* postLowShelfFrequency;
+    juce::AudioParameterFloat* postHighShelfGain;
+    juce::AudioParameterFloat* postHighShelfFrequency;
+    juce::AudioParameterFloat* postCutoffFrequency;
+
+    juce::AudioParameterFloat* tapSeed;
+    juce::AudioParameterFloat* diffusionSeed;
+    juce::AudioParameterFloat* delaySeed;
+    juce::AudioParameterFloat* postDiffusionSeed;
+    juce::AudioParameterFloat* crossSeed;    
+    
+    juce::AudioParameterFloat* hiPassEnabled;
+    juce::AudioParameterFloat* lowPassEnabled;
+    juce::AudioParameterFloat* lowShelfEnabled;
+    juce::AudioParameterFloat* highShelfEnabled;
+    juce::AudioParameterFloat* cutoffEnabled;
+    juce::AudioParameterFloat* lateStageTap;
+    juce::AudioParameterFloat* interpolation;
+	
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
 };
