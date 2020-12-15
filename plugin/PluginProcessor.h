09cf4afab7122b8de53d583d50cd29b31c0dc4a8
HolyBimBam <72328707+HolyBimBam@users.noreply.github.com>
2020-12-15 01:23:53 +0100

    Update PluginProcessor.h

--- plugin/PluginProcessor.h
+++ plugin/PluginProcessor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "../audio_engine/ReverbController.h"
+#include "audio_engine/ReverbController.h"
 
 //==============================================================================
 class AudioPluginAudioProcessor  : public juce::AudioProcessor
@@ -41,9 +41,104 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    //==============================================================================
+    
+    
+    //void AudioProcessorParameter::setValueNotifyHost(float);
+//    void loadPresetProgram (int index);
 
+    
+    void updateParameters();
+
+    int currentProgramNumber = {0};
+    
+    float inputMixSliderValue;
+    float predelaySliderValue;
+    float lowCutSliderValue;
+    float highCutSliderValue;
+    
+    float dryOutSliderValue;
+    float predelayOutSliderValue;
+    float earlyOutSliderValue;
+    float mainOutSliderValue;
+    
+    
+    float tapCountSliderValue;
+    float tapLengthSliderValue;
+    float diffusionDelaySliderValue;
+    float earlyDiffusionModAmountSliderValue;
+    
+    float tapGainSliderValue;
+    float tapDecaySliderValue;
+    float diffusionFeedbackSliderValue;
+    float earlyDiffusionModRateSliderValue;
+    
+    
+    float lineDelaySliderValue;
+    float lineModAmountSliderValue;
+    float lateDiffusionDelaySliderValue;
+    float lateDiffusionModAmountSliderValue;
+    
+    float lineDecaySliderValue;
+    float lineModRateSliderValue;
+    float lateDiffusionFeedbackSliderValue;
+    float lateDiffusionModRateSliderValue;
+    
+    
+    float postLowShelfFrequencySliderValue;
+    float postHighShelfFrequencySliderValue;
+    float postCutoffFrequencySliderValue;
+    
+    float postLowShelfGainSliderValue;
+    float postHighShelfGainSliderValue;
+    float crossSeedSliderValue;
+    
+    
+    float tapSeedSliderValue;
+    float diffusionSeedSliderValue;
+    float delaySeedSliderValue;
+    float postDiffusionSeedSliderValue;
+    
+
+    float lineCountSliderValue;
+    float diffusionStagesSliderValue;
+    float lateDiffusionStagesSliderValue;
+    
+    float diffusionEnabledButtonValue = {1.0f};
+    float lateDiffusionEnabledButtonValue = {1.0f};;
+
+    float hiPassEnabledButtonValue = {0.0f};
+    float lowPassEnabledButtonValue = {0.0f};
+    float cutoffEnabledButtonValue = {1.0f};
+    
+    float lowShelfEnabledButtonValue = {0.0f};
+    float highShelfEnabledButtonValue = {0.0f};
+    
+    float lateStageTapButtonValue = {1.0f};
+    float interpolationButtonValue = {0.0f};
+    
+    void initChorusPreset();
+//    void initFactoryDullEchos();
+//    void initFactoryHyperplane();
+//    void initFactoryMediumSpace();
+//    void initFactoryNoiseInTheHallway();
+//    void initFactoryRubiKaFields();
+//    void initFactorySmallRoom();
+//    void initFactory90sAreBack();
+//    void initFactoryThroughTheLookingGlass();
+    
+    
+    juce::AudioProcessorValueTreeState treeState;
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    
 private:
-	CloudSeed::ReverbController reverb;
+    CloudSeed::ReverbController reverb;
+    
+        
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
 };
+
+
